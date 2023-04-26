2538b5a42044e86f05490330421b164320004697
Ivan Levenhagen <70105678+Eckzzo@users.noreply.github.com>
2023-04-25 22:26:56 -0300

    feat(components): add

--- apps/web/src/components/MessageList.tsx
+++ apps/web/src/components/MessageList.tsx
@@ -1,5 +1,39 @@
-type MessageListProps = {};
+import { Send } from '@mui/icons-material';
+import { Box, IconButton, TextField } from '@mui/material';
 
-export const MessageList = (props: MessageListProps) => {
-	return <div></div>;
+type MessageListProps = {
+	children?: React.ReactNode;
+};
+
+export const MessageList = ({ children }: MessageListProps) => {
+	return (
+		<Box
+			sx={{
+				height: '100%',
+				display: 'flex',
+				flexDirection: 'column',
+				gap: 2,
+				justifyContent: 'flex-end',
+			}}
+		>
+			{children}
+			<Box sx={{ display: 'flex', gap: 1 }}>
+				<TextField
+					label="Message"
+					variant="outlined"
+					size="small"
+					sx={{ width: '100%' }}
+				/>
+				<IconButton
+					sx={{
+						color: '#FFFFFF',
+						backgroundColor: '#03d69d',
+						borderRadius: 0.5,
+					}}
+				>
+					<Send />
+				</IconButton>
+			</Box>
+		</Box>
+	);
 };
