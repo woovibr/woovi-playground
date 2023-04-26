9765d7ebb424dca33bb0e8b5fa8b38ee2f6e83c7
Fernando Silva <fernandonsilva16@gmail.com>
2023-04-25 23:30:36 -0300

    feat(message): list (#11)

--- apps/web/src/components/Message.tsx
+++ apps/web/src/components/Message.tsx
@@ -1,9 +1,20 @@
 import { Box, Card, Typography } from '@mui/material';
 import { WooviAvatar } from './WooviAvatar';
+import { useFragment } from 'react-relay';
+import { DateTime } from 'luxon';
 
-type MessageProps = {};
+type MessageProps = {
+	message: Message_message$key;
+};
 
 export const Message = (props: MessageProps) => {
+	const message = useFragment<Message_message$key>(graphql`
+		fragment Message_message on Message {
+			content
+			createdAt
+		}
+	`, props.message);
+
 	return (
 		<Card
 			variant="outlined"
@@ -13,10 +24,12 @@ export const Message = (props: MessageProps) => {
 				<WooviAvatar />
 				<Box sx={{ display: 'flex', flexDirection: 'column' }}>
 					<Typography fontWeight={500}>Woovi Playground</Typography>
-					<Typography variant="body2">Date</Typography>
+					<Typography variant="body2">
+						{DateTime.fromISO(message.createdAt).toFormat('dd/MM/yyyy HH:mm')}
+					</Typography>
 				</Box>
 			</Box>
-			<Typography variant="body2">Message</Typography>
+			<Typography variant="body2">{message.content}</Typography>
 		</Card>
 	);
 };
