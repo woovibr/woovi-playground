fa93dae8f6ed057e8b52cc9bfa5247969526d30c
Fernando Silva <fernandonsilva16@gmail.com>
2023-04-25 23:30:30 -0300

    feat(message): list

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,13 +1,58 @@
+import { GetServerSideProps } from 'next';
+
 import { Message } from '../components/Message';
 import { Layout } from '../components/Layout';
 import { MessageList } from '../components/MessageList';
+import { getPreloadedQuery } from '../relay/network';
+import { PreloadedQuery, graphql, usePreloadedQuery } from 'react-relay';
+import pageQuery, { pages_PageQuery } from '../__generated__/pages_PageQuery.graphql';
 
-export default function Web() {
+const IndexQuery = graphql`
+  query pages_PageQuery {
+    messages {
+			edges {
+				node {
+					id
+					...Message_message
+				}
+			}
+		}
+  }
+`;
+
+
+type IndexProps = {
+  queryRefs: {
+    pageQueryRef: PreloadedQuery<pages_PageQuery>;
+  };
+}
+
+const Index = ({ queryRefs }: IndexProps) => {
+	const data = usePreloadedQuery<pages_PageQuery>(IndexQuery, queryRefs.pageQueryRef);
+	
 	return (
 		<Layout>
 			<MessageList>
-				<Message />
+				{data.messages.edges.map(({ node }) => (
+					<Message key={node.id} message={node} />
+				))}
 			</MessageList>
 		</Layout>
 	);
 }
+
+export const getServerSideProps: GetServerSideProps = async context => {
+  return {
+    props: {
+      preloadedQueries: {
+        pageQueryRef: await getPreloadedQuery(
+          pageQuery,
+          {},
+        ),
+      },
+    },
+  };
+};
+
+
+export default Index;
